5dcf2b46f593a3e60e44fabc08261247005969b6
Lya Dalibard <77194352+lya-dali@users.noreply.github.com>
2021-05-21 12:11:29 +0200

    Delete test.js

--- days/day01/test.js
+++
@@ -1,9 +0,0 @@
-function helloWorld(capitalized){
-    let message = 'Hello World !';
-    message.toUpperCase();
-    console.log(message);
-};
-
-string.charAt(0).toUpperCase()
-
-helloWorld(capitalized);
