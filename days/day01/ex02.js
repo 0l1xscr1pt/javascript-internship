551e657b8cf802b8c53b333d8c7c8be6d2f2389b
Lya Dalibard <lyadalibard@gmail.com>
2021-05-24 15:54:34 +0200

    add: ex02, ex05

--- days/day01/ex02.js
+++ days/day01/ex02.js
@@ -1,7 +1,21 @@
-function printAlphabetReversed(){
-    console.log('Hello World !');
-};
+let alphabet = "";
 
-printAlphabetReversed();
+function printAlphabetReversed(capitelized, reverse) {
+    for (x = 97, i = String.fromCharCode(x); x <= 122; x++) {
+        alphabet += String.fromCharCode(x);
+    }
+    if (alphabet){
+        capitelized = alphabet.toUpperCase();
+        return capitelized;
+    }
+    reverse = alphabet.reduceRight((acc, curr) => acc.concat(curr));
+    return reverse;
+
+}
+
+console.log(printAlphabetReversed(alphabet));
 
 module.exports = {printAlphabetReversed};
+
+//function -25 lignes
+//split et reverse interdites
---
+++ days/day01/ex05.js
@@ -0,0 +1,24 @@
+function printComb () {
+    for (let a = 0; a <= 9; a++) {
+        return a;
+    }
+    //b = 1 -> 8 tours
+    // c = 2 -> 7 tours
+}
+
+console.log(printComb());
+
+//module.exports = {printComb}
+
+
+
+
+//ecrire fontion affiche dans ordre croissant
+//toutes differentes combi de 3 chiffres différents dans l'ordre croissant
+
+//fonction moins de 25 lignes
+
+//ex : 012, 013, 014, 015, 016, 017, 018, 019, 023, ..., 789
+// not 987 -> 789 present
+// not 999 -> 3 same nombers
+//not 020 -> 0 present x 2
