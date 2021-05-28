b648a4e81b9de34c7d4596f50213d565b5209642
Lya Dalibard <lyadalibard@gmail.com>
2021-05-28 12:59:11 +0200

    ex06 last commit

--- days/day01/ex06.js
+++ days/day01/ex06.js
@@ -1,7 +1,5 @@
-function reverseString(str){
+function reverseString(str) {
     return str.split("").reverse().join("");
 }
-
-console.log(reverseString("l'arbre"));
 
 module.exports = {reverseString}
