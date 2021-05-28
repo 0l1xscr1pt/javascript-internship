fc7903e07dc7490f5aed6a5617045c53bc0afb77
Lya Dalibard <lyadalibard@gmail.com>
2021-05-28 12:54:43 +0200

    ex01 last commit

--- days/day01/ex01.js
+++ days/day01/ex01.js
@@ -3,12 +3,11 @@ function printAlphabet(capitelized) {
     for (x = 97, i = String.fromCharCode(x); x <= 122; x++) {
         alphabet += String.fromCharCode(x);
     }
-    if (alphabet){
-    capitelized = alphabet.toUpperCase();
+    if (alphabet) {
+        capitelized = alphabet.toUpperCase();
         console.log(capitelized)
     }
 }
-printAlphabet();
 
 module.exports = {printAlphabet};
 
