a754b889439bce6f298debc9f725e596b48677ee
Lya Dalibard <lyadalibard@gmail.com>
2021-05-31 11:07:28 +0200

    ex02 commit 2

--- days/day01/ex02.js
+++ days/day01/ex02.js
@@ -3,15 +3,19 @@ function printAlphabetReversed(capitelized, reverse) {
     for (x = 97, i = String.fromCharCode(x); x <= 122; x++) {
         alphabet += String.fromCharCode(x);
     }
-    if (alphabet) {
+    if (capitelized === true) {
         capitelized = alphabet.toUpperCase();
+    } else {
+        console.log(alphabet);
     }
-    if (alphabet) {
+    if (reverse === true) {
         reverse = "";
         for (var i = capitelized.length - 1; i >= 0; i--) {
             reverse += capitelized[i];
         }
         console.log(reverse);
+    } else {
+        console.log(capitelized);
     }
 }
 
