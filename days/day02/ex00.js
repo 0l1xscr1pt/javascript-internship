86ad8d46edaae9efeb79c8132f9445485cd177e0
Lya Dalibard <lyadalibard@gmail.com>
2021-06-04 11:53:45 +0200

    ex00 commit 4

--- days/day02/ex00.js
+++ days/day02/ex00.js
@@ -1,9 +1,9 @@
 function atoi(numberAsString) {
     if (numberAsString > 1) {
-        return numberAsString
+        return numberAsString - '';
     } else if (numberAsString === 0) {
         return 0
     }
 }
-atoi(+ '0120')
+
 module.exports = {atoi}
