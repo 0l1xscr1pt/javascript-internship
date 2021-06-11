ce4cdad27423bee86ae83c83b71652764c53c8bb
Lya Dalibard <lyadalibard@gmail.com>
2021-06-11 15:28:32 +0200

    commit ex06

--- days/day02/ex06.js
+++ days/day02/ex06.js
@@ -1,19 +1,19 @@
 function keep(list, predicate) {
-    let newList = []
+    let newList = [];
     for (let n = 0; n < list.length; n++) {
         if (predicate(list[n])) {
-            newList.push(list[n])
+            newList.push(list[n]);
         }
     }
     return newList;
 }
 
 function discard(list, predicate) {
-    let newList = []
+    let newList = [];
     for (let n = 0; n < list.length; n++) {
         if (predicate(list[n]) == false) {
-            newList.push(list[n])
+            newList.push(list[n]);
         }
     }
-    return newList
+    return newList;
 }
