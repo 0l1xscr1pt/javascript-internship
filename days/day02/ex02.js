f9f09e4019ae7109abf539a913284eff3b3f7e34
Lya Dalibard <lyadalibard@gmail.com>
2021-06-16 14:44:31 +0200

    ex02 commit 02

--- days/day02/ex02.js
+++ days/day02/ex02.js
@@ -1,13 +1,13 @@
 function swap(o1, o2) {
     let ref1 = {
-        a: 1
+        a: o1
     };
 
     let ref2 = {
-        a: 5
+        a: o2
     };
 
-    [ref1.a, ref2.a] = [ref2.a, ref1.a];
+    return [ref1.a, ref2.a] = [ref2.a, ref1.a];
 }
 
-module.exports = {swap}
+module.exports = { swap }
